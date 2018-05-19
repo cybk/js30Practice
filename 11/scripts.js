1bf16c9a69d88d991e12f9e3f6240513b915e886
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-05-18 21:18:58 -0500

    Added the behavior for some controls

--- 11/scripts.js
+++ 11/scripts.js
@@ -1,13 +1,40 @@
 /* Get all the elements */
 const player = document.querySelector('.player');
-const video = document.querySelector('.video');
+const video = document.querySelector('.viewer');
 const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
 const toggle = document.querySelector('.toggle');
-const skipButtons = document.querySelector('[data-skip]');
-const rangers = document.querySelector('.player__slider');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const rangers = document.querySelectorAll('.player__slider');
 
 
 /* Build functions */
+function togglePlay(){
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton(){
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip(){
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+  video[this.name] = this.value;
+}
 
 /* Hook up the events listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(btn=> btn.addEventListener('click', skip));
+
+rangers.forEach(rng => rng.addEventListener('change', handleRangeUpdate));
+rangers.forEach(rng => rng.addEventListener('mousemove', handleRangeUpdate));

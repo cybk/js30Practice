60e70537996b6110be6f96421c3a1048f257dfb0
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-05-18 20:29:18 -0500

    Using js to get all the required elements

---
+++ 11/scripts.js
@@ -0,0 +1,13 @@
+/* Get all the elements */
+const player = document.querySelector('.player');
+const video = document.querySelector('.video');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelector('[data-skip]');
+const rangers = document.querySelector('.player__slider');
+
+
+/* Build functions */
+
+/* Hook up the events listeners */

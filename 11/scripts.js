bb05ddbb074fb623274e95c96490a62b97bc61cf
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-05-18 21:31:33 -0500

    Added the behavior for progress bar on video player

--- 11/scripts.js
+++ 11/scripts.js
@@ -6,7 +6,7 @@ const progressBar = document.querySelector('.progress__filled');
 const toggle = document.querySelector('.toggle');
 const skipButtons = document.querySelectorAll('[data-skip]');
 const rangers = document.querySelectorAll('.player__slider');
-
+progressBar.style.flexBasis = 0;
 
 /* Build functions */
 function togglePlay(){
@@ -27,10 +27,21 @@ function handleRangeUpdate(){
   video[this.name] = this.value;
 }
 
+function handleProgress(){
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 /* Hook up the events listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 
@@ -38,3 +49,9 @@ skipButtons.forEach(btn=> btn.addEventListener('click', skip));
 
 rangers.forEach(rng => rng.addEventListener('change', handleRangeUpdate));
 rangers.forEach(rng => rng.addEventListener('mousemove', handleRangeUpdate));
+
+let mouseDown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => { mouseDown && scrub(e); });
+progress.addEventListener('mousedown', () => mouseDown = true);
+progress.addEventListener('mouseup', () => mouseDown = false);

c33cd9af65b8b981ecde4059f26876655c654f6a
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-05-26 21:04:24 -0500

    added the functionality for display the hour expected

--- 29/scripts.js
+++ 29/scripts.js
@@ -1,10 +1,13 @@
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 let countDown;
 
 function timer(seconds){
   const date = Date.now();
   const then = date + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countDown = setInterval(() => {
     const secondsToLeft = Math.round((then - Date.now()) / 1000);
@@ -22,8 +25,15 @@ function timer(seconds){
 function displayTimeLeft(seconds){
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
-  const display = `${minutes}:${remainingSeconds}`;
-
-
+  const display = `${minutes}:${remainingSeconds < 10  ? '0' : ''}${remainingSeconds}`;
+  timerDisplay.textContent = display;
+  document.title = display;
   console.log(minutes, remainingSeconds);
+}
+
+function displayEndTime (timeStamp) {
+  const end = new Date(timeStamp);
+  const hours = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hours > 12 ? hours - 12 : hours}:${minutes < 10  ? '0' : ''}${minutes}`;
 }

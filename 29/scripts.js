4bc6e1efd554a3fdfae748a26f011b5e76b1aed7
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-05-26 21:09:14 -0500

    Added timers according with user selection on menu

--- 29/scripts.js
+++ 29/scripts.js
@@ -1,9 +1,12 @@
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 let countDown;
 
 function timer(seconds){
+  clearInterval(countDown);
+
   const date = Date.now();
   const then = date + seconds * 1000;
   displayTimeLeft(seconds);
@@ -37,3 +40,10 @@ function displayEndTime (timeStamp) {
   const minutes = end.getMinutes();
   endTime.textContent = `Be back at ${hours > 12 ? hours - 12 : hours}:${minutes < 10  ? '0' : ''}${minutes}`;
 }
+
+function startTimer(){
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach( button => button.addEventListener('click', startTimer));
